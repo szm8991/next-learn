9eea2570c0806e17387abcfd0feab8caf9f09f4c
songzheming <songzheming@163.com>
2023-12-07 21:27:04 +0800

    chore: a case of dynamic metadata

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,7 +1,39 @@
 import Frame from '@/components/frame';
 import swagPhotos, { Photo } from '@/photos';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
-export default function PhotoPage({ params: { id } }: { params: { id: string } }) {
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+  console.log(id);
+  // fetch data
+  const product = await new Promise<{ title: string }>(resolve =>
+    setTimeout(() => {
+      resolve({
+        title: 'A Dynamic Metadata',
+      });
+    }, 1500)
+  );
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.title,
+    openGraph: {
+      images: ['/E8txb2yVkAQxRVw.jfif', ...previousImages],
+    },
+  };
+}
+export default function PhotoPage({ params: { id } }: Props) {
   const photo: Photo | undefined = swagPhotos.find(p => p.id === id);
 
   if (!photo) {
